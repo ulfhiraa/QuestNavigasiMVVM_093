433d34a1b6c7349a86a6fe1d453d42396a318d3f
ulfhiraa <studyiraa@gmail.com>
2024-11-21 14:05:43 +0700

    menambahkan button kembali

--- app/src/main/java/com/example/pam_meet7/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_meet7/PengelolaHalaman.kt
@@ -6,7 +6,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
--- app/src/main/java/com/example/pam_meet7/ui/view/DataMahasiswaView.kt
+++ app/src/main/java/com/example/pam_meet7/ui/view/DataMahasiswaView.kt
@@ -3,8 +3,10 @@ package com.example.pam_meet7.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,7 +18,8 @@ import com.example.pam_meet7.model.DataMahasiswa
 fun DetailMahasiswaView(
     modifier: Modifier = Modifier,
     uiStateMahasiswa: DataMahasiswa,
-    )
+    onBackButton: () -> Unit
+)
 {
     val listDataMhs = listOf(
         Pair("Nama", uiStateMahasiswa.nama),
@@ -25,7 +28,17 @@ fun DetailMahasiswaView(
         Pair("NIM", uiStateMahasiswa.NIM)
     )
 
-    Column (){
+    Column (
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        ElevatedButton(
+            onClick = { onBackButton() }
+        ) {
+            Text("Kembali")
+        }
+
         listDataMhs.forEach { items ->
             CardSection(
                 judulParam = items.first,
