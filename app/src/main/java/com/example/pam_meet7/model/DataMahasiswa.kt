b6df60d01476fe05a9af781a9893bf8fc30674ee
ulfhiraa <studyiraa@gmail.com>
2024-11-21 13:05:35 +0700

    data class DataMahasiswa untuk menyimpan data nama, gender, alamat dan NIM

--- app/src/main/java/com/example/pam_meet7/model/DataMahasiswa.kt
+++ app/src/main/java/com/example/pam_meet7/model/DataMahasiswa.kt
@@ -3,5 +3,7 @@ package com.example.pam_meet7.model
 data class DataMahasiswa(
     val nama: String = "",
     val gender: String = "",
-    val alamat: String = ""
-)
+    val alamat: String = "",
+    val NIM: String = ""
+) // kelas data untuk menyimpan data nama, gender, alamat dan NIM
+// default "" akan otomatis diisi dengan string kosong.
